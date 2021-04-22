e00595fbbaa4b059cf154e12ce4251c953925f72
CCornez <cyrilcornez@outlook.com>
2021-04-22 19:14:44 +0200

    added js code

--- app.js
+++ app.js
@@ -1,51 +1,38 @@
-const task = document.querySelectorAll("main ul li a");
-const taskToArray = Array.apply(null, task);
-
-//NEED TO COMPACT THIS .TOGGLE
-
-const preventDefault = (event) => event.preventDefault();
-// const checkUncheck = () =>
-//   this.className === "checked"
-//     ? (this.className = "")
-//     : (this.className = "checked");
-
-taskToArray.forEach((v) => v.addEventListener("click", preventDefault));
-// taskToArray.forEach((v) => v.addEventListener("click", checkUncheck));
-
-taskToArray.forEach((v) =>
-  v.addEventListener("click", function () {
-    if (this.className === "checked") {
-      this.className = "";
-    } else {
-      this.className = "checked";
-    }
-  })
-);
-
-//---------------------------------------------------------------
-
 const taskList = [];
-let taskListHTML = "";
+let taskString = "";
 const formInputSelector = document.querySelector(".searchbar form input");
 const formButtonSelector = document.querySelector(".searchbar form button");
+const getTaskSelector = () => {
+  var taskSelector = document.querySelectorAll("main ul li a");
+  return (taskToArray = Array.apply(null, taskSelector));
+};
+
+const preventDefault = (event) => event.preventDefault();
+
+const addEventListenerInList = () => {
+  getTaskSelector();
+  taskToArray.forEach((v) => v.addEventListener("click", preventDefault));
+  taskToArray.forEach((v) =>
+    v.addEventListener("click", (e) => {
+      e.target.classList.toggle("checked");
+    })
+  );
+};
 
 const showList = () => {
-  taskListHTML = taskList
-    .map(
-      (v) =>
-        '<li><a href="#"><div class="checkmarkicon"><svg class="icon icon-checkmark"><use xlink:href="./icons/symbol-defs.svg#icon-checkmark"></use xlink:href=></svg></div><span>' +
-        v +
-        "</span></a></li>"
-    )
-    .reverse()
-    .join("");
-  document.querySelector("main ul").innerHTML = taskListHTML;
+  taskListHTML = taskList.reverse().join("");
+  document.querySelector("main ul").innerHTML = taskString;
+  addEventListenerInList();
 };
 
 const addInList = () => {
   let v = formInputSelector.value.trim();
   if (v.length > 0) {
     taskList.push(v);
+    taskString +=
+      '<li><a href="#"><div class="checkmarkicon"><svg class="icon icon-checkmark"><use xlink:href="./icons/symbol-defs.svg#icon-checkmark"></use xlink:href=></svg></div><span>' +
+      v +
+      "</span></a></li>";
     formInputSelector.value = "";
     showList();
   } else {
