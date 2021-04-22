3eed5d6df0d36cca44a9f35ad0923dfabe743650
CCornez <cyrilcornez@outlook.com>
2021-04-22 15:53:09 +0200

    possibility to add tasks added + checked/unchecked start

--- app.js
+++ app.js
@@ -1,15 +1,52 @@
 const task = document.querySelectorAll("main ul li a");
 const taskToArray = Array.apply(null, task);
 
-// function addEventListenerAll(element) {
-//   element.forEach((v) => v.addEventListener("click", taskCheck()));
-// }
-// function taskCheck() {
-//   this.className = "checked";
-// }
+//NEED TO COMPACT THIS .TOGGLE
+
+const preventDefault = (event) => event.preventDefault();
+// const checkUncheck = () =>
+//   this.className === "checked"
+//     ? (this.className = "")
+//     : (this.className = "checked");
+
+taskToArray.forEach((v) => v.addEventListener("click", preventDefault));
+// taskToArray.forEach((v) => v.addEventListener("click", checkUncheck));
 
 taskToArray.forEach((v) =>
   v.addEventListener("click", function () {
-    this.className = "checked";
+    if (this.className === "checked") {
+      this.className = "";
+    } else {
+      this.className = "checked";
+    }
   })
 );
+
+//---------------------------------------------------------------
+
+const taskList = [];
+let taskListHTML = "";
+const formInputSelector = document.querySelector(".searchbar form input");
+const formButtonSelector = document.querySelector(".searchbar form button");
+
+const showList = () => {
+  taskListHTML = taskList
+    .map(
+      (v) =>
+        '<li><a href="#"><div class="checkmarkicon"><svg class="icon icon-checkmark"><use xlink:href="./icons/symbol-defs.svg#icon-checkmark"></use xlink:href=></svg></div><span>' +
+        v +
+        "</span></a></li>"
+    )
+    .reverse()
+    .join("");
+  document.querySelector("main ul").innerHTML = taskListHTML;
+};
+
+const addInList = () => {
+  taskList.push(formInputSelector.value);
+  formInputSelector.value = "";
+  showList();
+};
+
+formButtonSelector.addEventListener("click", preventDefault);
+formButtonSelector.addEventListener("click", addInList);
