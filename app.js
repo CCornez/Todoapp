4c43fbe92ec1532a784470b59da04a767447e4ea
CCornez <cyrilcornez@outlook.com>
2021-04-24 15:45:24 +0200

    basis check/uncheck

--- app.js
+++ app.js
@@ -1,44 +1,89 @@
+function Task(task, checked = false) {
+  this.task = task;
+  this.checked = checked;
+}
+
 const taskList = [];
-let taskString = "";
-const formInputSelector = document.querySelector(".searchbar form input");
-const formButtonSelector = document.querySelector(".searchbar form button");
+const doneTaskList = [];
+const undoneTaskList = [];
+let doneTaskString = "";
+let undoneTaskString = "";
+
+// ------------------------------------------------------
 const getTaskSelector = () => {
   var taskSelector = document.querySelectorAll("main ul li a");
   return (taskToArray = Array.apply(null, taskSelector));
 };
+// ------------------------------------------------------
 
-const preventDefault = (event) => event.preventDefault();
+const formInputSelector = document.querySelector(".searchbar form input");
+const formButtonSelector = document.querySelector(".searchbar form button");
 
 const addEventListenerInList = () => {
   getTaskSelector();
-  taskToArray.forEach((v) => v.addEventListener("click", preventDefault));
-  taskToArray.forEach((v) =>
+  taskToArray.forEach((v) => {
+    v.addEventListener("click", preventDefault);
     v.addEventListener("click", (e) => {
       e.target.classList.toggle("checked");
-    })
-  );
+    });
+
+    //make more compact/efficient
+    v.addEventListener("click", (e) => {
+      e.target.classList.value === "checked"
+        ? (taskList.filter(
+            (v) => v.task === e.target.outerText
+          )[0].checked = true)
+        : (taskList.filter(
+            (v) => v.task === e.target.outerText
+          )[0].checked = false);
+      console.log(taskList);
+    });
+    v.addEventListener("click", filterTask);
+  });
 };
 
 const showList = () => {
-  taskListHTML = taskList.reverse().join("");
-  document.querySelector("main ul").innerHTML = taskString;
+  document.querySelector(".done").innerHTML = doneTaskString;
+  document.querySelector(".undone").innerHTML = undoneTaskString;
   addEventListenerInList();
+};
+
+const filterTask = () => {
+  doneTaskString = taskList
+    .filter((v) => v.checked)
+    .map(
+      (v) =>
+        '<li><a href="#" class="checked"><div class="checkmarkicon"><svg class="icon icon-checkmark"><use xlink:href="./icons/symbol-defs.svg#icon-checkmark"></use xlink:href=></svg></div><span>' +
+        v.task +
+        "</span></a></li>"
+    )
+    .reverse()
+    .join("");
+  undoneTaskString = taskList
+    .filter((v) => !v.checked)
+    .map(
+      (v) =>
+        '<li><a href="#"><div class="checkmarkicon"><svg class="icon icon-checkmark"><use xlink:href="./icons/symbol-defs.svg#icon-checkmark"></use xlink:href=></svg></div><span>' +
+        v.task +
+        "</span></a></li>"
+    )
+    .reverse()
+    .join("");
+  showList();
 };
 
 const addInList = () => {
   let v = formInputSelector.value.trim();
   if (v.length > 0) {
-    taskList.push(v);
-    taskString +=
-      '<li><a href="#"><div class="checkmarkicon"><svg class="icon icon-checkmark"><use xlink:href="./icons/symbol-defs.svg#icon-checkmark"></use xlink:href=></svg></div><span>' +
-      v +
-      "</span></a></li>";
+    taskList.push(new Task(v));
     formInputSelector.value = "";
-    showList();
+    filterTask();
   } else {
     formInputSelector.value = "";
   }
 };
+
+const preventDefault = (event) => event.preventDefault();
 
 formButtonSelector.addEventListener("click", preventDefault);
 formButtonSelector.addEventListener("click", addInList);
