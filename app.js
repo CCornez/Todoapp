dbf09ace0d1c31cc2960b40529b10afa30781df1
CCornez <cyrilcornez@outlook.com>
2021-04-25 15:39:39 +0200

    add count & rewrite check/uncheck function

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
-function Task(task, checked = false) {
+function Todo(task, checked = false) {
   this.task = task;
   this.checked = checked;
 }
 
 const taskList = [];
-const doneTaskList = [];
-const undoneTaskList = [];
+let doneTaskList = [];
+let undoneTaskList = [];
 let doneTaskString = "";
 let undoneTaskString = "";
 
 // ------------------------------------------------------
+
 const getTaskSelector = () => {
   var taskSelector = document.querySelectorAll("main ul li a");
   return (taskToArray = Array.apply(null, taskSelector));
 };
+const preventDefault = (event) => event.preventDefault();
+
 // ------------------------------------------------------
 
 const formInputSelector = document.querySelector(".searchbar form input");
 const formButtonSelector = document.querySelector(".searchbar form button");
+
+// ------------------------------------------------------
 
 const addEventListenerInList = () => {
   getTaskSelector();
@@ -26,20 +31,16 @@ const addEventListenerInList = () => {
     v.addEventListener("click", (e) => {
       e.target.classList.toggle("checked");
     });
-
-    //make more compact/efficient
     v.addEventListener("click", (e) => {
-      e.target.classList.value === "checked"
-        ? (taskList.filter(
-            (v) => v.task === e.target.outerText
-          )[0].checked = true)
-        : (taskList.filter(
-            (v) => v.task === e.target.outerText
-          )[0].checked = false);
+      let indexTask = taskList.map((v) => v.task).indexOf(e.target.outerText);
+      taskList[indexTask].checked = !taskList[indexTask].checked;
     });
     v.addEventListener("click", filterTask);
+    v.addEventListener("click", count);
   });
 };
+
+// ------------------------------------------------------
 
 const showList = () => {
   document.querySelector(".done").innerHTML = doneTaskString;
@@ -48,8 +49,9 @@ const showList = () => {
 };
 
 const filterTask = () => {
-  doneTaskString = taskList
-    .filter((v) => v.checked)
+  doneTaskList = taskList.filter((v) => v.checked);
+  undoneTaskList = taskList.filter((v) => !v.checked);
+  doneTaskString = doneTaskList
     .map(
       (v) =>
         '<li><a href="#" class="checked"><div class="checkmarkicon"><svg class="icon icon-checkmark"><use xlink:href="./icons/symbol-defs.svg#icon-checkmark"></use xlink:href=></svg></div><span>' +
@@ -58,8 +60,7 @@ const filterTask = () => {
     )
     .reverse()
     .join("");
-  undoneTaskString = taskList
-    .filter((v) => !v.checked)
+  undoneTaskString = undoneTaskList
     .map(
       (v) =>
         '<li><a href="#"><div class="checkmarkicon"><svg class="icon icon-checkmark"><use xlink:href="./icons/symbol-defs.svg#icon-checkmark"></use xlink:href=></svg></div><span>' +
@@ -71,10 +72,15 @@ const filterTask = () => {
   showList();
 };
 
+// ------------------------------------------------------
+
 const addInList = () => {
-  let v = formInputSelector.value.trim();
-  if (v.length > 0) {
-    taskList.push(new Task(v));
+  let input = formInputSelector.value.trim();
+  if (taskList.map((v) => v.task).includes(input)) {
+    formInputSelector.value = "";
+    console.log("task already exists");
+  } else if (input.length > 0) {
+    taskList.push(new Todo(input));
     formInputSelector.value = "";
     filterTask();
   } else {
@@ -82,7 +88,19 @@ const addInList = () => {
   }
 };
 
-const preventDefault = (event) => event.preventDefault();
+// ------------------------------------------------------
+
+const count = () => {
+  document.querySelector("main h2").innerHTML =
+    "Done (" + doneTaskList.length + ")";
+  document.querySelector("main h2:last-of-type").innerHTML =
+    "Remaining tasks: " + undoneTaskList.length;
+};
+
+// ------------------------------------------------------
 
 formButtonSelector.addEventListener("click", preventDefault);
 formButtonSelector.addEventListener("click", addInList);
+formButtonSelector.addEventListener("click", count);
+
+// ------------------------------------------------------
